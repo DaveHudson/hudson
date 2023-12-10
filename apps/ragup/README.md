03a9880a501eecd8645722feb9eda911e539960f
Dave Hudson <dave@applification.net>
2023-12-10 15:15:20 +0000

    RAG Uploader

--- apps/ragup/README.md
+++ apps/ragup/README.md
@@ -1,5 +1,7 @@
 # `ragup`
 
+Looks for Markdown documents in `/docs` directory. Chunks the files and uploads do Supabase Vector Store.
+
 ## Manage packages
 
 `pnpm add {package-name} --filter ragup`
@@ -7,3 +9,12 @@
 ## Development
 
 `npx turbo dev --filter ragup`
+
+## RAG
+
+This project uses Supabase with the vector extension to store RAG data, paired with LangChain to chunk & upload the documents to the vector databse.
+
+For more details, see the docs:
+
+- [https://js.langchain.com/docs/integrations/vectorstores/supabase](https://js.langchain.com/docs/integrations/vectorstores/supabase)
+- [https://scrimba.com/learn/langchain/supabase-setup-cocb84fce96a85fb50de02772](https://scrimba.com/learn/langchain/supabase-setup-cocb84fce96a85fb50de02772)
--- apps/ragup/index.ts
+++ apps/ragup/index.ts
@@ -1,13 +1,45 @@
+import "dotenv/config";
 import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
 import { TextLoader } from "langchain/document_loaders/fs/text";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { createClient } from "@supabase/supabase-js";
+import { SupabaseVectorStore } from "langchain/vectorstores/supabase";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 
-/* Insert documents from Obsidian, manually dropped into this repos docs folder */
+const sbApiKey = process.env.SUPABASE_API_KEY as string;
+const sbUrl = process.env.SUPABASE_URL as string;
+const openAIApiKey = process.env.OPENAI_API_KEY as string;
+
 const loader = new DirectoryLoader("./docs", {
   ".md": (path) => new TextLoader(path),
 });
 
 const docs = await loader.load();
 console.log("num docs", docs.length);
-console.log({ docs });
+
+const client = createClient(sbUrl, sbApiKey);
+
+// For each document in docs, split into chunks useing RecursivieCharacterTextSplitter from LangChain
+docs.forEach(async (doc) => {
+  try {
+    let text = doc.pageContent;
+    const splitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 500,
+      separators: ["\n\n", "\n", " ", "", "##"], // default setting (## popular in markdown)
+      chunkOverlap: 50,
+    });
+
+    const output = await splitter.createDocuments([text]);
+    console.log("Chunks", output.length);
+
+    // upload chunks to Supabase
+    await SupabaseVectorStore.fromDocuments(output, new OpenAIEmbeddings({ openAIApiKey }), {
+      client,
+      tableName: "documents",
+    });
+  } catch (err) {
+    console.error(err);
+  }
+});
 
 /* Crawl Applification Blog for content and insert */
