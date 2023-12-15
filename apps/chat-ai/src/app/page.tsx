864596492c3e7ce17e06a4142e9bec181b33ffa0
Dave Hudson <dave@applification.net>
2023-12-15 09:02:10 +0000

    Committing to AI SDK over LangChain

--- apps/chat-ai/src/app/page.tsx
+++ apps/chat-ai/src/app/page.tsx
@@ -1,23 +1,8 @@
-"use client";
-import PromptInput from "@repo/ui/prompt-input";
-import { useChat } from "ai/react";
-import Messages from "@repo/ui/messages";
+import { handler } from "./action";
+import { Chat } from "./chat";
 
-export default function Chat(): React.ReactNode {
-  const { input, handleInputChange, handleSubmit, messages } = useChat({
-    api: "/api/chat",
-  });
+export const runtime = "edge";
 
-  return (
-    <>
-      <div className="flex pb-4">
-        <Messages messages={messages} />
-      </div>
-      <div className="fixed bottom-0 left-0 right-0 z-10 bg-white dark:bg-slate-900">
-        <form className="flex w-full items-center rounded-md pl-1 pr-2 pb-6" onSubmit={handleSubmit}>
-          <PromptInput handleInputChange={handleInputChange} input={input} />
-        </form>
-      </div>
-    </>
-  );
+export default function Page() {
+  return <Chat handler={handler} />;
 }
--- apps/chat-ai/src/app/streaming/page.tsx
+++
@@ -1,8 +0,0 @@
-import { handler } from "../action";
-import { Chat } from "../chat";
-
-export const runtime = "edge";
-
-export default function Page() {
-  return <Chat handler={handler} />;
-}
