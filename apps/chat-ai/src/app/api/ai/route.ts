0968c67e06ed4fc90e930c9b2a83cdd0942725f5
Dave Hudson <dave@applification.net>
2023-12-15 07:59:56 +0000

    Streamlined version with reusable context util

--- apps/chat-ai/src/app/api/ai/route.ts
+++ apps/chat-ai/src/app/api/ai/route.ts
@@ -1,46 +1,26 @@
 import type { NextRequest } from "next/server";
 import type { Message } from "ai";
-import invariant from "tiny-invariant";
-import { createClient } from "@supabase/supabase-js";
-import { SupabaseVectorStore } from "langchain/vectorstores/supabase";
-import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import { OpenAIStream, StreamingTextResponse } from "ai";
 import OpenAI from "openai";
+import { getContext } from "../../utils/getContext";
 
 export const runtime = "edge";
 
-export function combineDocuments(docs) {
-  return docs.map((doc) => doc.pageContent).join("\n\n");
-}
-
-/*
- * This handler initializes and calls a simple chain with a prompt,
- * chat model, and output parser. See the docs for more information:
- *
- * https://js.langchain.com/docs/guides/expression_language/cookbook#prompttemplate--llm--outputparser
- */
 export async function POST(req: NextRequest) {
   const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
   });
 
+  // * Get all messages from the request
   const { messages } = await req.json();
+
+  // * Current message
   const currentMessageContent = messages[messages.length - 1].content;
 
-  invariant(process.env.SUPABASE_URL, "Missing SUPABASE_URL");
-  invariant(process.env.SUPABASE_SERVICE_ROLE_KEY, "Missing SUPABASE_SERVICE_ROLE_KEY");
+  // * Get RAG context
+  const context = await getContext(currentMessageContent);
 
-  const client = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
-
-  const vectorStore = await SupabaseVectorStore.fromExistingIndex(new OpenAIEmbeddings(), {
-    client,
-    tableName: "documents",
-    queryName: "match_documents",
-  });
-
-  const context = await vectorStore.similaritySearch(currentMessageContent, 3);
-
-  // RAG
+  // * Create Prompt with context
   const prompt = [
     {
       role: "system",
@@ -49,20 +29,22 @@ export async function POST(req: NextRequest) {
   Remember, your goal is to provide a conversational experience that is as close as possible to a real conversation with Dave. Do not invent or assume any views that are not explicitly stated in the context.
   Current conversation:
   {chat_history}  
-  Context: ${combineDocuments(context)}
+  Context: ${context}
   question: {question}
   answer: `,
     },
   ];
 
+  // * Send request to OpenAI
   const response = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
     stream: true,
     messages: [...prompt, ...messages.filter((message: Message) => message.role === "user")],
   });
 
-  // Convert the response into a friendly text-stream
+  // * Convert the response into a friendly text-stream
   const stream = OpenAIStream(response);
-  // Respond with the stream
+
+  // * Respond with the stream
   return new StreamingTextResponse(stream);
 }
---
+++ apps/chat-ai/src/app/utils/getContext.ts
@@ -0,0 +1,27 @@
+import invariant from "tiny-invariant";
+import { createClient } from "@supabase/supabase-js";
+import { SupabaseVectorStore } from "langchain/vectorstores/supabase";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+
+export function combineDocuments(docs) {
+  return docs.map((doc) => doc.pageContent).join("\n\n");
+}
+
+export async function getContext(message: string) {
+  invariant(process.env.SUPABASE_URL, "Missing SUPABASE_URL");
+  invariant(process.env.SUPABASE_SERVICE_ROLE_KEY, "Missing SUPABASE_SERVICE_ROLE_KEY");
+
+  const client = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
+
+  const vectorStore = await SupabaseVectorStore.fromExistingIndex(new OpenAIEmbeddings(), {
+    client,
+    tableName: "documents",
+    queryName: "match_documents",
+  });
+
+  const context = await vectorStore.similaritySearch(message, 3);
+
+  const combinedContext = combineDocuments(context);
+
+  return combinedContext;
+}
