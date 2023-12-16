8123c25d0728f9b6ddf78e8852605b125c1a6542
Dave Hudson <dave@applification.net>
2023-12-16 15:14:34 +0000

    Fix chat-ai styles path

--- apps/chat-ai/src/app/layout.tsx
+++ apps/chat-ai/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "@repo/ui/styles.css";
+import "@repo/ui/public/styles.css";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
