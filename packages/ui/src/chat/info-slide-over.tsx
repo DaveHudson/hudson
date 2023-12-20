75aed449fbf6962190a5819cefae25c07f692ff7
Dave Hudson <dave@applification.net>
2023-12-20 11:18:07 +0000

    UI tweaks

--- packages/ui/src/chat/info-slide-over.tsx
+++ packages/ui/src/chat/info-slide-over.tsx
@@ -7,12 +7,12 @@ import { cn } from "../../lib/utils";
 export default function InfoSlideOver({ open, setOpen }: { open: boolean; setOpen: (open: boolean) => void }) {
   return (
     <Transition.Root show={open} as={Fragment}>
-      <Dialog as="div" className="relative z-10" onClose={setOpen}>
-        <div className="fixed inset-0" />
+      <Dialog as="div" className="ui-relative ui-z-10" onClose={setOpen}>
+        <div className="ui-fixed ui-inset-0" />
 
-        <div className="fixed inset-0 overflow-hidden">
-          <div className="absolute inset-0 overflow-hidden">
-            <div className="pointer-events-none fixed inset-y-16 right-0 flex max-w-full pl-10 sm:pl-16">
+        <div className="ui-fixed ui-inset-0 ui-overflow-hidden">
+          <div className="ui-absolute ui-inset-0 ui-overflow-hidden">
+            <div className="ui-pointer-events-none ui-fixed ui-inset-y-16 ui-right-0 ui-flex ui-max-w-full ui-pl-10 sm:ui-pl-16">
               <Transition.Child
                 as={Fragment}
                 enter="transform transition ease-in-out duration-500 sm:duration-700"
@@ -22,46 +22,49 @@ export default function InfoSlideOver({ open, setOpen }: { open: boolean; setOpe
                 leaveFrom="translate-x-0"
                 leaveTo="translate-x-full"
               >
-                <Dialog.Panel className="pointer-events-auto w-screen max-w-md">
-                  <div className="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
-                    <div className="px-4 py-6 sm:px-6">
-                      <div className="flex items-start justify-between">
-                        <h2 id="slide-over-heading" className="text-base font-semibold leading-6 text-gray-900">
+                <Dialog.Panel className="ui-pointer-events-auto ui-w-screen ui-max-w-md">
+                  <div className="ui-flex ui-h-full ui-flex-col ui-overflow-y-scroll ui-bg-white ui-shadow-xl">
+                    <div className="ui-px-4 ui-py-6 sm:ui-px-6">
+                      <div className="ui-flex ui-items-start ui-justify-between">
+                        <h2
+                          id="slide-over-heading"
+                          className="ui-text-base ui-font-semibold ui-leading-6 ui-text-gray-900"
+                        >
                           Open AI
                         </h2>
-                        <div className="ml-3 flex h-7 items-center">
+                        <div className="ui-ml-3 ui-flex ui-h-7 ui-items-center">
                           <button
                             type="button"
-                            className="relative rounded-md bg-white text-gray-400 hover:text-gray-500 focus:ring-2 focus:ring-sky-500"
+                            className="ui-relative ui-rounded-md ui-bg-white ui-text-gray-400 hover:ui-text-gray-500 focus:ui-ring-2 focus:ui-ring-sky-500"
                             onClick={() => setOpen(false)}
                           >
-                            <span className="absolute -inset-2.5" />
-                            <span className="sr-only">Close panel</span>
-                            <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                            <span className="ui-absolute ui--inset-2.5" />
+                            <span className="ui-sr-only">Close panel</span>
+                            <XMarkIcon className="ui-h-6 ui-w-6" aria-hidden="true" />
                           </button>
                         </div>
                       </div>
                     </div>
                     {/* Main */}
                     <div>
-                      <div className="pb-1 sm:pb-6">
+                      <div className="ui-pb-1 sm:ui-pb-6">
                         <div>
-                          <div className="mt-6 px-4 sm:mt-8 sm:flex sm:items-end sm:px-6">
-                            <div className="sm:flex-1">
+                          <div className="ui-mt-6 ui-px-4 sm:ui-mt-8 sm:ui-flex sm:ui-items-end sm:ui-px-6">
+                            <div className="sm:ui-flex-1">
                               <div>
-                                <div className="flex items-center">
-                                  <h3 className="text-xl font-bold text-gray-900 sm:text-2xl">Chat GPT</h3>
-                                  <span className="ml-2.5 inline-block h-2 w-2 flex-shrink-0 rounded-full bg-green-400">
-                                    <span className="sr-only">Online</span>
+                                <div className="ui-flex ui-items-center">
+                                  <h3 className="ui-text-xl ui-font-bold ui-text-gray-900 sm:ui-text-2xl">Chat GPT</h3>
+                                  <span className="ui-ml-2.5 ui-inline-block ui-h-2 ui-w-2 ui-flex-shrink-0 ui-rounded-full ui-bg-green-400">
+                                    <span className="ui-sr-only">Online</span>
                                   </span>
                                 </div>
-                                <p className="text-sm text-gray-500">gpt-3.5-turbo-instruct</p>
+                                <p className="ui-text-sm ui-text-gray-500">gpt-3.5-turbo-instruct</p>
                               </div>
-                              <div className="mt-5 flex flex-wrap space-y-3 sm:space-x-3 sm:space-y-0">
+                              <div className="ui-mt-5 ui-flex ui-flex-wrap ui-space-y-3 sm:ui-space-x-3 sm:ui-space-y-0">
                                 <a href="https://openai.com/chatgpt" target="_blank">
                                   <button
                                     type="button"
-                                    className="inline-flex w-full flex-shrink-0 items-center justify-center rounded-md bg-sky-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600 sm:flex-1"
+                                    className="ui-inline-flex ui-w-full ui-flex-shrink-0 ui-items-center ui-justify-center ui-rounded-md !ui-bg-sky-600 ui-px-3 ui-py-2 ui-text-sm ui-font-semibold ui-text-white ui-shadow-sm hover:!ui-bg-sky-500 focus-visible:ui-outline focus-visible:ui-outline-2 focus-visible:ui-outline-offset-2 focus-visible:ui-outline-sky-600 sm:ui-flex-1"
                                   >
                                     Website
                                   </button>
@@ -69,17 +72,17 @@ export default function InfoSlideOver({ open, setOpen }: { open: boolean; setOpe
                                 <a href="https://platform.openai.com/docs/introduction" target="_blank">
                                   <button
                                     type="button"
-                                    className="inline-flex w-full flex-1 items-center justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                                    className="ui-inline-flex ui-w-full ui-flex-1 ui-items-center ui-justify-center ui-rounded-md ui-bg-white ui-px-3 ui-py-2 ui-text-sm ui-font-semibold ui-text-gray-900 ui-shadow-sm ui-ring-1 ui-ring-inset ui-ring-gray-300 hover:ui-bg-gray-50"
                                   >
                                     Docs
                                   </button>
                                 </a>
-                                <div className="ml-3 inline-flex sm:ml-0">
-                                  <Menu as="div" className="relative inline-block text-left">
-                                    <Menu.Button className="relative inline-flex items-center rounded-md bg-white p-2 text-gray-400 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-                                      <span className="absolute -inset-1" />
-                                      <span className="sr-only">Open options menu</span>
-                                      <EllipsisVerticalIcon className="h-5 w-5" aria-hidden="true" />
+                                <div className="ui-ml-3 ui-inline-flex sm:ui-ml-0">
+                                  <Menu as="div" className="ui-relative ui-inline-block ui-text-left">
+                                    <Menu.Button className="ui-relative ui-inline-flex ui-items-center ui-rounded-md ui-bg-white ui-p-2 ui-text-gray-400 ui-shadow-sm ui-ring-1 ui-ring-inset ui-ring-gray-300 hover:ui-bg-gray-50">
+                                      <span className="ui-absolute ui--inset-1" />
+                                      <span className="ui-sr-only">Open options menu</span>
+                                      <EllipsisVerticalIcon className="ui-h-5 ui-w-5" aria-hidden="true" />
                                     </Menu.Button>
                                     <Transition
                                       as={Fragment}
@@ -90,15 +93,15 @@ export default function InfoSlideOver({ open, setOpen }: { open: boolean; setOpe
                                       leaveFrom="transform opacity-100 scale-100"
                                       leaveTo="transform opacity-0 scale-95"
                                     >
-                                      <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                        <div className="py-1">
+                                      <Menu.Items className="ui-absolute ui-right-0 ui-z-10 ui-mt-2 ui-w-48 ui-origin-top-right ui-rounded-md ui-bg-white ui-shadow-lg ui-ring-1 ui-ring-black ui-ring-opacity-5 focus:ui-outline-none">
+                                        <div className="ui-py-1">
                                           <Menu.Item>
                                             {({ active }) => (
                                               <a
                                                 href="#"
                                                 className={cn(
-                                                  active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                                                  "block px-4 py-2 text-sm"
+                                                  active ? "ui-bg-gray-100 ui-text-gray-900" : "ui-text-gray-700",
+                                                  "ui-block ui-px-4 ui-py-2 ui-text-sm"
                                                 )}
                                               >
                                                 View profile
@@ -110,8 +113,8 @@ export default function InfoSlideOver({ open, setOpen }: { open: boolean; setOpe
                                               <a
                                                 href="#"
                                                 className={cn(
-                                                  active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                                                  "block px-4 py-2 text-sm"
+                                                  active ? "ui-bg-gray-100 ui-text-gray-900" : "ui-text-gray-700",
+                                                  "ui-block ui-px-4 ui-py-2 ui-text-sm"
                                                 )}
                                               >
                                                 Copy profile link
@@ -128,26 +131,32 @@ export default function InfoSlideOver({ open, setOpen }: { open: boolean; setOpe
                           </div>
                         </div>
                       </div>
-                      <div className="px-4 pb-5 pt-5 sm:px-0 sm:pt-0">
-                        <dl className="space-y-8 px-4 sm:space-y-6 sm:px-6">
+                      <div className="ui-px-4 ui-pb-5 ui-pt-5 sm:ui-px-0 sm:ui-pt-0">
+                        <dl className="ui-space-y-8 ui-px-4 sm:ui-space-y-6 sm:ui-px-6">
                           <div>
-                            <dt className="text-sm font-medium text-gray-500 sm:w-40 sm:flex-shrink-0">Info</dt>
-                            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2">
+                            <dt className="ui-text-sm ui-font-medium ui-text-gray-500 sm:ui-w-40 sm:ui-flex-shrink-0">
+                              Info
+                            </dt>
+                            <dd className="ui-mt-1 ui-text-sm ui-text-gray-900 sm:ui-col-span-2">
                               <p>Chat with Open AI Chat GPT gpt-3.5-turbo-instruct model.</p>
                             </dd>
                           </div>
                           <div>
-                            <dt className="text-sm font-medium text-gray-500 sm:w-40 sm:flex-shrink-0">Prompt</dt>
-                            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2">
-                              <pre className="whitespace-pre-wrap">
+                            <dt className="ui-text-sm ui-font-medium ui-text-gray-500 sm:ui-w-40 sm:ui-flex-shrink-0">
+                              Prompt
+                            </dt>
+                            <dd className="ui-mt-1 ui-text-sm ui-text-gray-900 sm:ui-col-span-2">
+                              <pre className="ui-whitespace-pre-wrap">
                                 You are a helpful assistant. All responses must be succinct and to the point. Current
                                 conversation: `{`chat_history`}` User: `{`input`}` AI:
                               </pre>
                             </dd>
                           </div>
                           <div>
-                            <dt className="text-sm font-medium text-gray-500 sm:w-40 sm:flex-shrink-0">Temperature</dt>
-                            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2">0.8</dd>
+                            <dt className="ui-text-sm ui-font-medium ui-text-gray-500 sm:ui-w-40 sm:ui-flex-shrink-0">
+                              Temperature
+                            </dt>
+                            <dd className="ui-mt-1 ui-text-sm ui-text-gray-900 sm:ui-col-span-2">0.8</dd>
                           </div>
                         </dl>
                       </div>
