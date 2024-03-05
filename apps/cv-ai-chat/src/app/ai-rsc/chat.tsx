ce2d472413f830b3cd139e53e366fd1d2ad4d1da
Dave Hudson <dave@applification.net>
2024-03-05 10:11:53 +0000

    functionality alignment

--- apps/cv-ai-chat/src/app/ai-rsc/chat.tsx
+++ apps/cv-ai-chat/src/app/ai-rsc/chat.tsx
@@ -1,17 +1,13 @@
 "use client";
 
-import { XCircleIcon } from "@heroicons/react/20/solid";
 import { Button } from "@repo/ui/base/button";
 import { Select } from "@repo/ui/catalyst/select";
-import Messages from "@repo/ui/messages";
-import PromptInput from "@repo/ui/prompt-input";
-import { Spinner } from "@repo/ui/spinner";
-import { useChat } from "ai/react";
 import { useActions, useUIState } from "ai/rsc";
 import { useEffect, useRef, useState } from "react";
-import invariant from "tiny-invariant";
 import { AI } from "./action";
 import MessageUser from "@repo/ui/message-user";
+import { useEnterSubmit } from "../utils/hooks/use-enter-submit";
+import Textarea from "react-textarea-autosize";
 
 export function Chat() {
   const [prompt, setPrompt] = useState("CV Chat");
@@ -20,7 +16,9 @@ export function Chat() {
   const [sourcesUI, setSourcesUI] = useState<null | React.ReactNode>(null);
   const [systemUI, setSystemUI] = useState<null | React.ReactNode>(null);
 
-  const textareaRef = useRef(null);
+  const { formRef, onKeyDown } = useEnterSubmit();
+  const inputRef = useRef<HTMLTextAreaElement>(null);
+  const [inputValue, setInputValue] = useState("");
 
   useEffect(() => {
     setMessages([]);
@@ -45,45 +43,7 @@ export function Chat() {
               </div>
             ))}
           </div>
-        ) : (
-          <>
-            <div className="mt-4 flex w-full gap-x-2 overflow-x-auto whitespace-nowrap text-xs text-slate-600 dark:text-slate-300 md:text-sm">
-              {prompts.map((prompt) => {
-                return (
-                  <Button
-                    className="mb-2"
-                    variant="outline"
-                    size="sm"
-                    key={prompt}
-                    onClick={async (e) => {
-                      e.preventDefault();
-                      const button = e.target as HTMLButtonElement;
-                      // add user message to UI
-                      setMessages((currentMessages) => [
-                        ...currentMessages,
-                        {
-                          id: Date.now(),
-                          display: <MessageUser>{`${button.textContent}`}</MessageUser>,
-                        },
-                      ]);
-
-                      // submit and get response message
-                      const responseMessage = await submitUserMessage(`${button.textContent}`);
-                      setMessages((currentMessages) => [...currentMessages, responseMessage]);
-
-                      // submit and get sources & system UI
-                      const responseSources = await getSourceContext(`${button.textContent}`);
-                      setSourcesUI(responseSources.sourcesUI);
-                      setSystemUI(responseSources.systemUI);
-                    }}
-                  >
-                    {prompt}
-                  </Button>
-                );
-              })}
-            </div>
-          </>
-        )}
+        ) : null}
         {sourcesUI ? (
           <>
             <div>{sourcesUI}</div>
@@ -97,13 +57,47 @@ export function Chat() {
       </div>
       <br />
       <div className="fixed bottom-0 w-11/12 md:w-1/2 z-10 bg-white dark:bg-slate-900">
-        {/* <form
-          onSubmit={(e) => {
-            handleSubmit(e, {
-              data: {
-                prompt,
+        <form
+          ref={formRef}
+          onSubmit={async (e) => {
+            e.preventDefault();
+
+            // Blur focus on mobile
+            if (window.innerWidth < 600) {
+              e.target["message"]?.blur();
+            }
+
+            const value = inputValue.trim();
+            setInputValue("");
+            if (!value) return;
+
+            // get the form data
+            const formData = new FormData(e.target as HTMLFormElement);
+            // convert FormData to an object
+            const formObject = Object.fromEntries(formData.entries());
+            console.log("form data", formObject);
+            // use Object.entries to get all form values
+            for (const [key, value] of Object.entries(formObject)) {
+              console.log(`${key}: ${value}`);
+            }
+
+            // add user message to UI
+            setMessages((currentMessages) => [
+              ...currentMessages,
+              {
+                id: Date.now(),
+                display: <MessageUser>{`${value}`}</MessageUser>,
               },
-            });
+            ]);
+
+            // submit and get response message
+            const responseMessage = await submitUserMessage(`${value}`);
+            setMessages((currentMessages) => [...currentMessages, responseMessage]);
+
+            // submit and get sources & system UI
+            const responseSources = await getSourceContext(`${value}`);
+            setSourcesUI(responseSources.sourcesUI);
+            setSystemUI(responseSources.systemUI);
           }}
         >
           <div className="flex flex-col items-center rounded-md">
@@ -111,7 +105,33 @@ export function Chat() {
               <div className="mt-4 flex w-full gap-x-2 overflow-x-auto whitespace-nowrap text-xs text-slate-600 dark:text-slate-300 md:text-sm">
                 {prompts.map((prompt) => {
                   return (
-                    <Button className="mb-2" variant="outline" size="sm" onClick={() => setInput(prompt)} key={prompt}>
+                    <Button
+                      className="mb-2"
+                      variant="outline"
+                      size="sm"
+                      key={prompt}
+                      onClick={async (e) => {
+                        e.preventDefault();
+                        const button = e.target as HTMLButtonElement;
+                        // add user message to UI
+                        setMessages((currentMessages) => [
+                          ...currentMessages,
+                          {
+                            id: Date.now(),
+                            display: <MessageUser>{`${button.textContent}`}</MessageUser>,
+                          },
+                        ]);
+
+                        // submit and get response message
+                        const responseMessage = await submitUserMessage(`${button.textContent}`);
+                        setMessages((currentMessages) => [...currentMessages, responseMessage]);
+
+                        // submit and get sources & system UI
+                        const responseSources = await getSourceContext(`${button.textContent}`);
+                        setSourcesUI(responseSources.sourcesUI);
+                        setSystemUI(responseSources.systemUI);
+                      }}
+                    >
                       {prompt}
                     </Button>
                   );
@@ -136,23 +156,24 @@ export function Chat() {
                   <option>Cover Letter</option>
                 </Select>
               </div>
-              <PromptInput
-                textareaRef={textareaRef}
-                handleInputChange={handleInputChange}
-                input={input}
-                disabled={isLoading}
-                placeholder={
-                  prompt === "CV Chat"
-                    ? "Chat with AI about my CV"
-                    : prompt === "CV Match"
-                      ? "Paste in a job description to see if it is a good match"
-                      : "Paste in a job description to generate a personalised cover letter"
-                }
-                fileUpload={false}
+              <Textarea
+                ref={inputRef}
+                tabIndex={0}
+                onKeyDown={onKeyDown}
+                placeholder="Send a message."
+                className="min-h-[60px] w-full resize-none bg-transparent px-4 py-[1.3rem] focus-within:outline-none sm:text-sm"
+                autoFocus
+                spellCheck={false}
+                autoComplete="off"
+                autoCorrect="off"
+                name="message"
+                rows={1}
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
               />
             </div>
           </div>
-        </form> */}
+        </form>
       </div>
     </div>
   );
