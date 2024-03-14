f4b645fd97222caa513b69f912ab3b67dad5a99f
Dave Hudson <dave@applification.net>
2024-03-14 13:07:21 +0000

    Add thumbs up and thumbs down functionality to MessageAIRSC component

--- packages/ui/src/message-ai-rsc.tsx
+++ packages/ui/src/message-ai-rsc.tsx
@@ -1,8 +1,12 @@
 "use client";
+import { useState } from "react";
 import { Avatar } from "./catalyst/avatar";
 import profile from "./profile.jpg";
 
 export default function MessageAIRSC({ children }: { children: string | JSX.Element }) {
+  const [thumbsUpColor, setThumbsUpColor] = useState("currentColor");
+  const [thumbsDownColor, setThumbsDownColor] = useState("currentColor");
+
   return (
     <div className="ui-flex">
       <div className="ui-flex ui-flex-row ui-px-4 ui-y-1">
@@ -12,11 +16,18 @@ export default function MessageAIRSC({ children }: { children: string | JSX.Elem
       <div className="ui-flex ui-w-full ui-flex-col ui-items-start">
         <div className="ui-w-full ui-pb-2">{children}</div>
         <div className="ui-mt-4 ui-flex ui-flex-row ui-justify-start ui-gap-x-2 ui-text-slate-500 lg:ui-mt-0">
-          <button className="hover:ui-text-sky-500" type="button">
+          <button
+            className="hover:ui-text-sky-500"
+            type="button"
+            onClick={() => {
+              setThumbsUpColor("#fff");
+              setThumbsDownColor("#64748B");
+            }}
+          >
             <svg
               className="ui-h-5 ui-w-5"
               fill="none"
-              stroke="currentColor"
+              stroke={thumbsUpColor}
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth="2"
@@ -27,11 +38,18 @@ export default function MessageAIRSC({ children }: { children: string | JSX.Elem
               <path d="M7 11v8a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1v-7a1 1 0 0 1 1 -1h3a4 4 0 0 0 4 -4v-1a2 2 0 0 1 4 0v5h3a2 2 0 0 1 2 2l-1 5a2 3 0 0 1 -2 2h-7a3 3 0 0 1 -3 -3" />
             </svg>
           </button>
-          <button className="hover:ui-text-sky-500" type="button">
+          <button
+            className="hover:ui-text-sky-500"
+            type="button"
+            onClick={() => {
+              setThumbsUpColor("#64748B");
+              setThumbsDownColor("#fff");
+            }}
+          >
             <svg
               className="ui-h-5 ui-w-5"
               fill="none"
-              stroke="currentColor"
+              stroke={thumbsDownColor}
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth="2"
@@ -40,36 +58,6 @@ export default function MessageAIRSC({ children }: { children: string | JSX.Elem
             >
               <path d="M0 0h24v24H0z" fill="none" stroke="none" />
               <path d="M7 13v-8a1 1 0 0 0 -1 -1h-2a1 1 0 0 0 -1 1v7a1 1 0 0 0 1 1h3a4 4 0 0 1 4 4v1a2 2 0 0 0 4 0v-5h3a2 2 0 0 0 2 -2l-1 -5a2 3 0 0 0 -2 -2h-7a3 3 0 0 0 -3 3" />
-            </svg>
-          </button>
-          <button
-            className="hover:ui-text-sky-500"
-            onClick={() => {
-              console.log(children);
-              navigator.clipboard.writeText(`${children}`).then(
-                () => {
-                  console.log("Copying to clipboard was successful!");
-                },
-                (err) => {
-                  console.error("Could not copy text: ", err);
-                }
-              );
-            }}
-            type="button"
-          >
-            <svg
-              className="ui-h-5 ui-w-5"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M0 0h24v24H0z" fill="none" stroke="none" />
-              <path d="M8 8m0 2a2 2 0 0 1 2 -2h8a2 2 0 0 1 2 2v8a2 2 0 0 1 -2 2h-8a2 2 0 0 1 -2 -2z" />
-              <path d="M16 8v-2a2 2 0 0 0 -2 -2h-8a2 2 0 0 0 -2 2v8a2 2 0 0 0 2 2h2" />
             </svg>
           </button>
         </div>
