bade25dd90f7a32765758b81146ce80d81553a41
Dave Hudson <dave@applification.net>
2024-03-14 13:30:45 +0000

    Update layout to include full screen height

--- apps/cv-ai-chat/src/app/layout.tsx
+++ apps/cv-ai-chat/src/app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }): JSX.Element {
   return (
-    <html lang="en">
+    <html className="h-screen" lang="en">
       <body className={`${inter.className} h-full flex flex-row !dark:bg-slate-100`}>
         <main className="w-full md:w-3/5">{children}</main>
         <aside className="hidden w-0 md:w-2/5 md:block bg-slate-900 border-l-2 overflow-auto">
